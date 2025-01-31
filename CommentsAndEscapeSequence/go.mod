77f1ef5d85444f9e278e150fad7a2b43586ac258
REZ-OAN <ahmedabir.rez@gmail.com>
2025-01-31 09:43:43 +0600

    added CommentsAndEscapeSequence examples

---
+++ CommentsAndEscapeSequence/go.mod
@@ -0,0 +1,3 @@
+module REZ0AN/comments-and-escape-sequence
+
+go 1.23.1
---
+++ CommentsAndEscapeSequence/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	/**
+	* Comments
+	multiple lines
+
+	*/
+	// Single line comment
+	fmt.Println("Hello, World!")
+	// escape sequence
+	// newline
+	fmt.Println("Hello, \nWorld!")
+	// tab
+	fmt.Println("Hello, \tWorld!")
+	// backspace
+	fmt.Println("Hello, \bWorld!")
+	// carriage return
+	fmt.Println("Hello, \rWorld!")
+	// form feed
+	fmt.Println("Hello, \fWorld!")
+	// single quote
+	fmt.Println("Hello, 'World!")
+	// double quote
+	fmt.Println("Hello, \"World!")
+	// backslash
+	fmt.Println("Hello, \\World!")
+	// percent
+	fmt.Println("Hello, %World!")
+	// octal
+	fmt.Println("Hello, \141World!")
+	// hexadecimal
+	fmt.Println("Hello, \x61World!")
+	// unicode
+	fmt.Println("Hello, \u0061World!")
+	// unicode
+	fmt.Println("Hello, \U00000061World!")
+
+}
